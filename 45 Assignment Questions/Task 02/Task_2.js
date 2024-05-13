97cf8ac8da08dfec26d8b625d864d63dfed1f6b5
Aliyan Shaikh <150450858+aliyannshaikhh@users.noreply.github.com>
2024-05-13 18:43:10 +0500

    Add files via upload

---
+++ 45 Assignment Questions/Task 02/Task_2.js
@@ -0,0 +1,3 @@
+//Task No. 2: Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+var my_name = "Aliyan Shaikh";
+console.log("Hello ".concat(my_name, ", would you like to learn some Python today."));
---
+++ 45 Assignment Questions/Task 10/Task_10.ts
@@ -0,0 +1,19 @@
+//Task No. 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+//Task No. 7 performs 4 Arithmetic Operations.
+
+console.log("Addition:", 5 + 3); //Addition
+
+console.log("Subtraction:", 10 - 2); //Subtraction
+
+console.log("Multiplication:", 4 * 2); //Multiplication
+
+console.log("Division:", 16 / 2); //Division
+
+//Task No. 9 reveals my favorite number.
+
+var favorite_number:number = 6; //6 is my favorite number since my name contains six letters in it.
+
+let message:string = `${favorite_number} is my favorite number`;
+
+console.log(message);
