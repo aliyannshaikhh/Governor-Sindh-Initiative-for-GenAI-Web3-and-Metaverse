353917a442e728ee000acc64700e3febdb8b99b0
Aliyan Shaikh <150450858+aliyannshaikhh@users.noreply.github.com>
2024-05-13 19:01:07 +0500

    Add files via upload

---
+++ 45 Assignment Questions/Task 22/Task_22.ts
@@ -0,0 +1,9 @@
+//Task No. 22: Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+let num_array:number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 9];
+
+//Accessing index 11 which does not exist.
+
+//console.log(num_array[11]);
+
+console.log(num_array[10]) //Making it correct.
