9279049546de3643a8e4d279fb95a169a9c67915
Aliyan Shaikh <150450858+aliyannshaikhh@users.noreply.github.com>
2024-05-13 18:14:55 +0500

    Add files via upload

---
+++ 45 Assignment Questions/Task 5/Task_5.ts
@@ -0,0 +1,7 @@
+//Task No. 5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person:string = "Adele Adkins";
+
+let message:string = `${famous_person} once said, \"The greatest gift we can give ourselves is the permission to fully embrace our authentic selves.\"`;
+
+console.log(message);
